49461b3257c263d1ceb721f07c8c0ce603a0deea
hiro <kckyy292@gmail.com>
2022-08-02 23:33:59 +0900

    ABOUT更新

--- src/component/page/about.js
+++ src/component/page/about.js
@@ -11,51 +11,36 @@ class About extends React.Component {
                 <h1 className="p-title"><Link to={`/portfolio-react/`}>Portfolio</Link></h1>
                 <section>
                     <h2 className="p-sub_title">ABOUT</h2>
-                    <p className="home_description">ECサイトの運営会社で、フロントエンドエンジニアとしてLP制作やSEO対策・UI改善・新規機能の開発などを行なっています。<br />常にユーザーおよび事業にどのような価値をもたらすのかを考え、有意義なUXとなるように最善を尽くすことでストレスのない最高のプロダクトを提供できるエンジニアになりたいです。<span>※詳細については職務経歴書をご覧ください。</span></p>
-                    <div className="l-contents_area about c-contents_area">
-                        <div>
-                            <p className="about_title">愛媛大学農学部</p>
-                            <p className="about_description">食品免疫学について研究する。国際会議で発表をするなど積極的に研究に取り組む。
-                            <span className="about_note_title">関連論文(名前は旧姓です)：</span>
-                            <span className="about_note">1) Ishida, M., Nishi, K., <span>Kunihiro, N.</span>, Onda, H., Nishimoto, S., & Sugahara, T. (2017). Immunostimulatory effect of aqueous extract of Coriandrum sativum L. seed on macrophages. Journal of the Science of Food and Agriculture, 97(14), 4727-4736.
-                            <br/>2) Ishida, M., Nishi, K., Shinohara, K., <span>Kunihiro, N.</span>, Osajima, K., Suemitsu, T., & Sugahara, T. (2017). Immunostimulatory effect of dried bonito extract on mouse macrophage cell lines and mouse primary peritoneal macrophages. Cytotechnology, 69(2), 229-244.</span></p>
-                        </div>
+                    <div className="l-contents_area c-contents_area p-about">
+                        常にユーザーおよび事業にどのような価値をもたらすのかを考え、有意義なUXとなるように最善を尽くすことでストレスのない最高のプロダクトを提供できるエンジニアになりたいと考えております。
                         <hr/>
-                        <div>
-                            <p className="about_title">SES企業</p>
-                            <p className="about_description">大学卒業後に入社しシステム開発を行なっていました。業務外にCRUD機能を持つ簡単なアプリを実装したり、Java Silverの認定資格を取得するなど積極的に技術力を高める努力をしました。フロントエンドエンジニアとしてのキャリアを築いていきたいと思い転職しました。</p>
-                        </div>
-                        <hr/>
-                        <div>
-                            <p className="about_title">ECサイト運営会社</p>
-                            <p className="about_description">現在はフロントエンドエンジニアとして、主にLP制作やSEO対策・UI改善・新規機能の開発などを行なっています。1年以上かけて行なったSEO対策ではサイト訪問者数を咋対比123%までに増やすことに成功しました。常に「ユーザー視点で考える」ことを意識しています。</p>
-                        </div>
-                    </div>
-                    <div className="l-contents_area p-about_content">
-                        {/* 矢印 */}
-                        <div className="p-about_content__arrow_wrap">
-                            <span className="p-about_content__arrow"></span>
-                            <span className="p-about_content__arrow_point point1"></span>
-                            <span className="p-about_content__arrow_point point2"></span>
-                            <span className="p-about_content__arrow_point point3"></span>
-                        </div>
-                        <div className="">
-                            <div>
-                                <p className="about_title">愛媛大学農学部</p>
-                                <p className="about_description">食品免疫学について研究する。国際会議で発表をするなど積極的に研究に取り組む。
-                                <span className="about_note_title">関連論文(名前は旧姓です)：</span>
-                                <span className="about_note">1) Ishida, M., Nishi, K., <span>Kunihiro, N.</span>, Onda, H., Nishimoto, S., & Sugahara, T. (2017). Immunostimulatory effect of aqueous extract of Coriandrum sativum L. seed on macrophages. Journal of the Science of Food and Agriculture, 97(14), 4727-4736.
+                        <div className="p-about__content">
+                            <p className="p-about__date">2012年~2016年</p>
+                            <div className="p-about__textwrap">
+                                <p className="p-about__title">愛媛大学農学部</p>
+                                <p className="p-about__description">食品免疫学について研究する。国際会議で発表をするなど積極的に研究に取り組む。
+                                <span className="p-about__note_title">関連論文(名前は旧姓です)：</span>
+                                <span className="p-about__note">1) Ishida, M., Nishi, K., <span>Kunihiro, N.</span>, Onda, H., Nishimoto, S., & Sugahara, T. (2017). Immunostimulatory effect of aqueous extract of Coriandrum sativum L. seed on macrophages. Journal of the Science of Food and Agriculture, 97(14), 4727-4736.
                                 <br/>2) Ishida, M., Nishi, K., Shinohara, K., <span>Kunihiro, N.</span>, Osajima, K., Suemitsu, T., & Sugahara, T. (2017). Immunostimulatory effect of dried bonito extract on mouse macrophage cell lines and mouse primary peritoneal macrophages. Cytotechnology, 69(2), 229-244.</span></p>
                             </div>
-                            <div>
-                                <p className="about_title">SES企業</p>
-                                <p className="about_description">大学卒業後に入社しシステム開発を行なっていました。業務外にCRUD機能を持つ簡単なアプリを実装したり、Java Silverの認定資格を取得するなど積極的に技術力を高める努力をしました。フロントエンドエンジニアとしてのキャリアを築いていきたいと思い転職しました。</p>
-                            </div>
-                            <div>
-                                <p className="about_title">ECサイト運営会社</p>
-                                <p className="about_description">現在はフロントエンドエンジニアとして、主にLP制作やSEO対策・UI改善・新規機能の開発などを行なっています。1年以上かけて行なったSEO対策ではサイト訪問者数を咋対比123%までに増やすことに成功しました。常に「ユーザー視点で考える」ことを意識しています。</p>
+                        </div>
+                        <hr/>
+                        <div className="p-about__content">
+                            <p className="p-about__date">2016年~2018年</p>
+                            <div className="p-about__textwrap">
+                                <p className="p-about__title">SES企業</p>
+                                <p className="p-about__description">大学卒業後に入社しシステム開発を行なっていました。業務外にCRUD機能を持つ簡単なアプリを実装したり、Java Silverの認定資格を取得するなど積極的に技術力を高める努力をしました。フロントエンドエンジニアとしてのキャリアを築いていきたいと思い転職しました。</p>
                             </div>
                         </div>
+                        <hr/>
+                        <div className="p-about__content">
+                            <p className="p-about__date">2019年~現在</p>
+                            <div className="p-about__textwrap">
+                                <p className="p-about__title">ECサイト運営会社</p>
+                                <p className="p-about__description">現在はフロントエンドエンジニアとして、LP制作やSEO対策・UI改善・新規機能の開発などを行なっています。1年以上かけて行なったSEO対策ではサイト訪問者数を咋対比123%までに増やすことに成功しました。</p>
+                            </div>
+                        </div>
+                        <p className="p-about__more">※詳細については職務経歴書をご覧ください。</p>
                     </div>
                     <ul className="p-contents_nav">
                         <li>
